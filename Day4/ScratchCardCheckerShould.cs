18135f13ef1704645957d62ce9459d21740e90e0
Andy Beetz <andy.beetz@spektrix.com>
2024-07-08 13:48:34 +0100

    Solving Day4 part 2

--- Day4/ScratchCardCheckerShould.cs
+++ Day4/ScratchCardCheckerShould.cs
@@ -39,9 +39,34 @@ public class ScratchCardCheckerShould
     }
     
     [Test]
+    public void Count_The_Number_Of_Cards()
+    {
+        var scratchCard = """
+                          Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                          Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                          Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                          Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                          Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                          Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+                          """;
+
+        var cards = ScratchCardChecker.CountCards(scratchCard);
+        
+        Assert.That(cards, Is.EqualTo(30));
+    }
+    
+    [Test]
     public void Solve_Part1()
     {
         var points = ScratchCardChecker.CheckCards(Part1Input.Input);
+        
+        Assert.That(points, Is.LessThan(0));
+    }
+    
+    [Test]
+    public void Solve_Part2()
+    {
+        var points = ScratchCardChecker.CountCards(Part1Input.Input);
         
         Assert.That(points, Is.LessThan(0));
     }
@@ -58,6 +83,23 @@ public static partial class ScratchCardChecker
     {
         return SumOf(SplitOnNewLine(scratchCards),
             card => CalculatePoints(FromNumberOfWinningNumbers(OfScratchCard(card))));
+    }
+    
+    public static int CountCards(string scratchCardStrings)
+    {
+        var scratchCards = SplitOnNewLine(scratchCardStrings).Select(OfScratchCard).ToList();
+        var numberOfCards = scratchCards.Count;
+        var cards = Enumerable.Repeat(1, numberOfCards).ToArray();
+        for (var card = 0; card < numberOfCards; card++)
+        {
+            var scratchCard = scratchCards[card];
+            for (var additionalCards = 1; additionalCards <= FromNumberOfWinningNumbers(scratchCard); additionalCards++)
+            {
+                cards[card + additionalCards] += cards[card];
+            }
+        }
+
+        return cards.Sum();
     }
 
     private static double SumOf(IEnumerable<string> scratchCards, Func<string, double> calculation) =>
